855262c20d187e31d8cb6a4a4e6eabc445ccb16a
Guillaume GAUJAC <ggaujacpro@gmail.com>
2017-02-12 16:04:10 +0100

    First commit

---
+++ app/src/main/java/com/example/guillaume/naviguationdrawer/DrivingSchoolFragment.java
@@ -0,0 +1,25 @@
+package com.example.guillaume.naviguationdrawer;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Guillaume on 12/02/2017.
+ */
+
+public class DrivingSchoolFragment extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.divenfragment,container,false);
+        return view;
+    }
+
+
+
+}
